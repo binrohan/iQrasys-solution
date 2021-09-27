6ec814a2c607fa937df7e776820df65eb9a4f579
Bin Rohan <binrohan.cs@gmail.com>
2021-09-28 01:07:12 +0600

    refactoring

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -25,6 +25,25 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleSidebar();
   });
 
+  
+window.addEventListener('scroll', (e) => {
+  const nav = document.querySelector('nav');
+  const navMid = document.querySelector('.nav-mid');
+  const navBottom = document.querySelector('.nav-bottom');
+
+  if(window.scrollY > 25){
+      nav.classList.add('sticky');
+      if(window.innerWidth > 1040){
+          navMid.classList.add('nav-mid--compact');
+          navBottom.classList.add('hide');
+      }
+  } else {
+      nav.classList.remove('sticky');
+      navMid.classList.remove('nav-mid--compact');
+      navBottom.classList.remove('hide');
+  }
+});
+
   const toggleSidebar = function () { 
     const bodyEl = document.querySelector('body');
 
